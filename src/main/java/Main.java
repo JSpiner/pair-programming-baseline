7337e0fc57ae7b893bafb5671f76fb294c2d03da
JSpiner <jspiner@naver.com>
2018-11-23 19:04:11 +0900

    baseline 추가

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        ElevatorSimulator simulator = new ElevatorSimulator();
+
+        int count = simulator.evaluateCount(3, new int[]{11, 10, 9, 10, 10, 11, 11, 11, 11});
+        System.out.println("count : " + count);
+    }
+}
+
+class ElevatorSimulator {
+
+    int evaluateCount(int m, int[] floors) {
+        // TODO : 로직 구현해보기!
+        return 0;
+    }
+
+}
